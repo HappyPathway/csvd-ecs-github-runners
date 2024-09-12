cd414640357d63279802010ea37b7368373fdeea
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-09-12 15:26:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,92 +3,17 @@ resource "aws_ecs_cluster" "github-runner" {
 }
 
 
-data "aws_iam_policy_document" "admin" {
-  statement {
-    sid = "1"
+resource "aws_ecs_cluster_capacity_providers" "fargate" {
+  cluster_name = aws_ecs_cluster.github-runner.name
 
-    actions = [
-      "*"
-    ]
+  capacity_providers = ["FARGATE"]
 
-    resources = [
-      "*",
-    ]
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
   }
 }
-
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "ssh-access-ecs-instances-${var.namespace}"
-  description = "Security group to allow SSH from everywhere"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh"
-  }
-}
-
-
-module "ecs_cluster_instances" {
-  source         = "HappyPathway/instance/aws"
-  instance_count = var.cluster_size
-  security_group_ids = concat(
-    var.security_groups,
-    [
-      aws_security_group.allow_ssh.id
-    ]
-  )
-  subnet        = var.subnets[0]
-  ssh_user      = "ec2-user"
-  instance_type = "t2.micro"
-  ami           = "ami-05c643bcdf4b2ae88"
-  required_tags = {
-    Name = aws_ecs_cluster.github-runner.name
-  }
-  root_block_device = {
-    volume_size = 100
-  }
-  ebs_block_devices = [
-    {
-      device_name           = "/dev/sdb"
-      volume_size           = 100
-      volume_type           = "gp2"
-      delete_on_termination = true
-    }
-  ]
-  troubleshoot     = false
-  project_name     = var.namespace
-  windows_instance = false
-  iam_policy       = data.aws_iam_policy_document.admin.json
-  store_key        = true
-  secret_path      = "/ssh-keys/ecs-clusters/${var.namespace}"
-  config = {
-    content = templatefile("${path.root}/ecs_cluster_init.sh",
-      {
-        cluster_name = aws_ecs_cluster.github-runner.name
-    }),
-    script = "/opt/config.sh"
-    args   = ""
-  }
-  depends_on = [
-    aws_ecs_cluster.github-runner
-  ]
-}
-
 
 locals {
   labels = [
